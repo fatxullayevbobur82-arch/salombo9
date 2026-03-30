938a7b431eb446d8c30fc020d5ebc3d15308408a
fatxullayevbobur82-arch <fatxullayevbobur82@gmail.com>
2026-03-30 14:16:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const firstName = prompt("Ismingizni kiriting:");
+const lastName = prompt("Familyangizni kiriting:");
+const age = prompt("Yoshingizni kiriting:");
+const address = prompt("Qayerda yashaysiz?");
+const subject = prompt("Yoqtirgan faningizni kiriting:");
+
+console.log("Foydalanuvchi ma'lumotlari:");
+console.log("Ismi: " + firstName);
+console.log("Familyasi: " + lastName);
+console.log("Yoshi: " + age);
+console.log("Manzili: " + address);
+console.log("Yoqtirgan fani: " + subject);
